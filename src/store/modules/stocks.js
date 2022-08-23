fc6e2f7faac51122bc729ee3f6a1c5deef22662b
JacksonSantin <jackdhanyelsn@gmail.com>
2022-08-23 13:37:42 -0300

    Stock Trader - Vue.js

---
+++ src/store/modules/stocks.js
@@ -0,0 +1,34 @@
+import stocks from "@/data/stocks";
+
+export default {
+  state: {
+    stocks: [],
+  },
+  mutations: {
+    setStocks(state, stocks) {
+      state.stocks = stocks;
+    },
+    randomizeStocks(state) {
+      state.stocks.forEach((stock) => {
+        stock.price = Math.round(stock.price * (1 + Math.random() - 0.45));
+      });
+    },
+  },
+  actions: {
+    buyStock({ commit }, order) {
+      commit("buyStock", order);
+    },
+    initStocks({ commit }) {
+      // console.log("initStocks...");
+      commit("setStocks", stocks);
+    },
+    randomizeStocks({ commit }) {
+      commit("randomizeStocks");
+    },
+  },
+  getters: {
+    stocks(state) {
+      return state.stocks;
+    },
+  },
+};
